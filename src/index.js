11f7113425ba0d08ca063b6bdf25637da02322d9
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-18 02:32:03 +0500

    Add Footer and update the dist folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//     const element = document.createElement('div');
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
   
-    return element;
-  }
+//     return element;
+//   }
   
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
