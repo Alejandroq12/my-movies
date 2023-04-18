dc169d9adf1ba4dd8cd0d9e1c9cc906c295e02d2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-18 23:17:46 +0500

    Create popup section and update dist folder

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,33 +1,21 @@
-// import _ from 'lodash';
-import './style.css';
-const API = [
-    {
-        id : '1',
-        name: 'movie',
-        description: '3 hours long',
-        img: 'img',
-        length: '100',
-        fuel: 'no fuel'
-}
-]
 // pop-up section
 const commentBtn = document.querySelectorAll('.commentBtn');
 const popMenu = document.querySelector('.seePopup');
 
-commentBtn.forEach((btn, index) => {
+seeProject.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     document.body.classList.toggle('no-scroll');
     popMenu.classList.add('act');
     popMenu.innerHTML = `
     <section id="commentPopup">
   <div class="imgContainer">
-    <img class= "commentImg" src="${API[index].img}" alt="Comment Image"><span class="close-btn">&#x2715;</span>
+    <img class= "commentImg" src="/assets/Book1.png" alt="Comment Image"><span class="close-btn">&#x2715;</span>
   </div>
-  <h3 id="commentTitle">${API[index].name}</h3>
+  <h3 id="commentTitle">Space 3</h3>
 <div id="commentDetails">
-  <p>Fuel: ${API[index].fuel}</p>
-  <p>Length: ${API[index].length}</p>
-  <p>Weight: ${API[index].length}</p>
+  <p>Fuel: titanium</p>
+  <p>Length: 100,000</p>
+  <p>Weight: 400</p>
   <p>Power: 100,000,000</p>
 </div>
 </section>`;
