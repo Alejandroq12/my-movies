c8558b2b148360eb86b22f8726abd1ad2ae80eb8
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-18 14:10:04 -0600

    Add heart icon

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ function createCardComponent(index, imgSrc = '', text = '') {
         <div class="card-content">
           <p class="card-text">${text}</p>
           <div class="likes-container">
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              class="card-likes"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path
+                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
+              ></path>
+            </svg>
             <span>5 Likes</span>
           </div>
         </div>
