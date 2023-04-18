8d8f4b39e00898fdee78b3ccbe500632f4d3d705
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-17 20:42:45 -0600

    Update dist folder

--- dist/main.js
+++ dist/main.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n// import _ from 'lodash';\r\n\r\n\r\n// function component() {\r\n//     const element = document.createElement('div');\r\n  \r\n//     // Lodash, now imported by this script\r\n//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');\r\n//     element.classList.add('hello');\r\n  \r\n//     return element;\r\n//   }\r\n  \r\n//   document.body.appendChild(component());\n\n//# sourceURL=webpack://webpack-installation/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n// import _ from 'lodash';\r\n\r\n\r\n// function component() {\r\n//     const element = document.createElement('div');\r\n  \r\n//     // Lodash, now imported by this script\r\n//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');\r\n//     element.classList.add('hello');\r\n  \r\n//     return element;\r\n//   }\r\n  \r\n//   document.body.appendChild(component());\r\n\n\n//# sourceURL=webpack://webpack-installation/./src/index.js?");
 
 /***/ })
 
