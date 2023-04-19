b491826ec454f7db9b928856468dcb8cfda017ad
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-19 10:02:25 -0600

    Add function to show notification, and to attach event listener  to the button

--- src/index.js
+++ src/index.js
@@ -98,7 +98,59 @@ function updateLikesCount(likesData) {
   });
 }
 
+function attachLikeButtonListener() {
+  const appId = 'p01X0Mr4syDGinD4IhgC';
+  const likeButtons = document.querySelectorAll('.card-likes');
 
+  const showNotification = (message) => {
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.textContent = message;
+    document.body.appendChild(notification);
+    setTimeout(() => {
+      notification.classList.add('show');
+    }, 50);
+    setTimeout(() => {
+      notification.classList.remove('show');
+      setTimeout(() => {
+        notification.remove();
+      }, 300);
+    }, 2000);
+  };
+
+  likeButtons.forEach((likeButton) => {
+    likeButton.addEventListener('click', async (event) => {
+      const card = event.target.closest('.container_card');
+      const itemId = card.id;
+
+      const response = await fetch(`${involvementApiBaseURL}/apps/${appId}/likes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          item_id: itemId,
+        }),
+      });
+
+      if (response.ok) {
+        // Update the likes count on the screen
+        const likesSpan = card.querySelector('.likes-container span');
+        const currentLikes = parseInt(likesSpan.textContent.split(' ')[0], 10);
+        likesSpan.textContent = `${currentLikes + 1} Likes`;
+
+        // Change the heart icon color to red and show the notification
+        event.target.style.stroke = 'red';
+        showNotification('Like added!');
+
+        // Revert the heart icon color back to the original after 2 seconds
+        setTimeout(() => {
+          event.target.style.stroke = 'currentColor';
+        }, 2000);
+      }
+    });
+  });
+}
 
 async function main() {
   const appId = 'p01X0Mr4syDGinD4IhgC';
