719aa0d3043eb7264399c740565884cd38cf01f2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-04-20 03:36:19 +0500

    Update commentCounter.js

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -5,15 +5,9 @@ const commentCounter = {
       const data = await response.json();
       return data.length;
     } catch (error) {
-      const errorMessage = 'There was an error while fetching comments data.';
-      // Display error message to the user instead of logging to console
-      const errorContainer = document.createElement('div');
-      errorContainer.classList.add('error-message');
-      errorContainer.textContent = errorMessage;
-      const commentSection = document.getElementById('commentSection');
-      commentSection.appendChild(errorContainer);
-      return 0; // Return a default value if there's an error
+      console.error(error);
     }
+    return 0;
   },
 };
 
