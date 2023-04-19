c62df7e1bbdfaca614180b0507af1345967c091c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-18 19:48:26 -0600

    Add function to update likes counter

--- src/index.js
+++ src/index.js
@@ -81,7 +81,21 @@ async function fetchLikes(appId) {
   }
 }
 
+function updateLikesCount(likesData) {
+  const likeElements = document.querySelectorAll('.likes-container span');
 
+  likeElements.forEach((likeElement) => {
+    const itemId = likeElement.parentElement.parentElement.parentElement.id;
+
+    const foundItem = likesData.find((item) => item.item_id === itemId);
+
+    if (foundItem) {
+      likeElement.textContent = `${foundItem.likes} Likes`;
+    } else {
+      likeElement.textContent = '0 Likes';
+    }
+  });
+}
 
 
 async function main() {
