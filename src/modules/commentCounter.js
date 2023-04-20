a9859dc66d7088b72fab1c5f17da89ee840c1c57
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-20 12:17:59 -0600

    Update files to fix bug with comment count

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -3,10 +3,11 @@ const commentCounter = {
     try {
       const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/p01X0Mr4syDGinD4IhgC/comments?item_id=${showId}`);
       const data = await response.json();
-      return { data: data.length, error: null };
+      return data.length;
     } catch (error) {
-      return { data: 0, error };
+      // console.error(error);
     }
+    return 0;
   },
 };
 
