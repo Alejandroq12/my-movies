dc8ec0b6e4f2a4ed79464aae6f3ae03e01d9ca65
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-21 00:37:15 -0600

    Add animations and keyframes  to the page

--- src/modules/createCardComponent.js
+++ src/modules/createCardComponent.js
@@ -20,7 +20,7 @@ const createCardComponent = (id, imgSrc = '', text = '', likes = 0) => `
                   d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
                 ></path>
               </svg>
-              <span>${likes} Likes</span>
+              <span class='likes-text'>${likes} Likes</span>
             </div>
           </div>
           <button
